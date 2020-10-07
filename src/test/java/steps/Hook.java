9a5db68bc362ac0933ab84f2f61a4e74377200e0
akasemi15 <69328895+akasemi15@users.noreply.github.com>
2020-10-07 18:00:21 -0500

    Update Hook.java

--- src/test/java/steps/Hook.java
+++ src/test/java/steps/Hook.java
@@ -40,11 +40,11 @@ public class Hook extends BaseUtil{
 
 
         //Chrome driver
-//        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
-//        base.Driver = new ChromeDriver();
-
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/geckodriver");
         base.Driver = new ChromeDriver();
+
+       // System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
+        //base.Driver = new ChromeDriver();
     }
 
 
